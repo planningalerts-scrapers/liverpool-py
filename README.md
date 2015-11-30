db81113138e69d8c21b0dcd3ac116b28a5ea5386
Henare Degan <henare.degan@gmail.com>
2015-11-30 12:37:15 +1100

    Replace scraper with PHP one by @LoveMyData
    https://github.com/LoveMyData/liverpool

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+Liverpool City Council Scraper
+-----------------------------
+
+Liverpool City Council site is very easy to scrape, only need to scape a single page to get the entire month of DAs. No pagination, yay... Three cheers to  this council !!!
+
+However, the lack of HTML class, id, etc to identify the data, it requires a few 'explode' in PHP to get the right information. Not a big deal as I am getting better at it now.
+
+Thanks and Enjoy
---
+++ scraper.php
@@ -0,0 +1,64 @@
+<?php
+### Liverpool City Council scraper
+
+require 'scraperwiki.php'; 
+require 'simple_html_dom.php';
+
+date_default_timezone_set('Australia/Sydney');
+
+$url_base = "http://eplanning.liverpool.nsw.gov.au";
+$comment_base = "http://www.liverpool.nsw.gov.au/eplanningportal/contact";
+
+# Default to 'thismonth', use MORPH_PERIOD to change to 'lastmonth'
+if (empty(getenv('MORPH_PERIOD'))) {
+    $da_page = $url_base . "/Pages/XC.Track/SearchApplication.aspx?d=thismonth&k=LodgementDate";
+} else {
+    $da_page = $url_base . "/Pages/XC.Track/SearchApplication.aspx?d=" .getenv('MORPH_PERIOD'). "&k=LodgementDate";
+}
+
+$mainUrl = scraperWiki::scrape("$da_page");
+$dom = new simple_html_dom();
+$dom->load($mainUrl);
+
+# Just focus on the a section of the web site
+$dataset = $dom->find("div[id=hiddenresult] div[class=result]");
+
+# The usual, look for the data set and if needed, save it
+foreach($dataset as $record) {
+    # Slow way to transform the date but it works
+    $date_received = explode('<br />', trim($record->find("div", 0)->innertext));
+    $date_received = preg_replace('/\s+/', ' ', $date_received[0]);
+    $date_received = explode(' ', $date_received);
+    $date_received = explode('/', trim($date_received[1]));
+    $date_received = "$date_received[2]-$date_received[1]-$date_received[0]";
+    
+    # Prep some data before hand
+    $desc = explode('<br />', $record->innertext);
+    $desc = explode('-', $desc[1], 2);
+    $desc = html_entity_decode($desc[1]);
+    $desc = trim(preg_replace('/\s+/', ' ', $desc));
+    $desc = ucwords(strtolower($desc));    
+
+    # Put all information in an array
+    $application = array (
+        'council_reference' => trim($record->find("a",0)->plaintext),
+        'address' => trim(html_entity_decode($record->find("strong",0)->plaintext)) . "  AUSTRALIA",
+        'description' => $desc,
+        'info_url' => $url_base . substr($record->find("a",0)->href, 5),
+        'comment_url' => $comment_base,
+        'date_scraped' => date('Y-m-d'),
+        'date_received' => date('Y-m-d', strtotime($date_received))
+    );
+
+    # Check if record exist, if not, INSERT, else do nothing
+    $existingRecords = scraperwiki::select("* from data where `council_reference`='" . $application['council_reference'] . "'");
+    if (count($existingRecords) == 0) {
+        print ("Saving record " . $application['council_reference'] . "\n");
+        # print_r ($application);
+        scraperwiki::save(array('council_reference'), $application);
+    } else {
+        print ("Skipping already saved record " . $application['council_reference'] . "\n");
+    }
+}
+
+?>
