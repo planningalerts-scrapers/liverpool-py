cd1a14c88627cbaad8f78eeaa1bbee62839b7b1f
Eric Tam <eric.tam@traceyanderic.com>
2018-11-13 15:43:57 +1100

    Initial write with ruby's scraper

---
+++ scraper.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
+
+case ENV['MORPH_PERIOD']
+  when 'lastmonth'
+  	period = "lastmonth"
+  when 'thismonth'
+  	period = "thismonth"
+  when
+    period = "thisweek"
+  else
+    period = "last14days"
+end
+puts "Getting data in `" + period + "`, changable via MORPH_PERIOD environment"
+
+scrape_icon_rest_xml("https://eplanning.liverpool.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=" + period + "&k=LodgementDate&o=xml", false)
