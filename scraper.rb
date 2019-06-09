185b71b26879dbb319a2f516ee2b960642a6882c
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 01:58:18 +1000

    Update to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,3 @@
 require "icon_scraper"
 
-IconScraper.rest_xml(
-  "https://eplanning.liverpool.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx",
-  "d=last14days&k=LodgementDate&o=xml",
-  false
-)
+IconScraper.scrape_and_save(:liverpool)
