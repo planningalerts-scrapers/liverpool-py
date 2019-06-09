2b804c14b60a44b49fe74275a50b62aeaf0df28c
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 01:52:25 +1000

    Get rid of MORPH_PERIOD temporarily

--- scraper.rb
+++ scraper.rb
@@ -1,15 +1,7 @@
 require "icon_scraper"
 
-case ENV['MORPH_PERIOD']
-  when 'lastmonth'
-  	period = "lastmonth"
-  when 'thismonth'
-  	period = "thismonth"
-  when
-    period = "thisweek"
-  else
-    period = "last14days"
-end
-puts "Getting data in `" + period + "`, changable via MORPH_PERIOD environment"
-
-IconScraper.rest_xml("https://eplanning.liverpool.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=" + period + "&k=LodgementDate&o=xml", false)
+IconScraper.rest_xml(
+  "https://eplanning.liverpool.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx",
+  "d=last14days&k=LodgementDate&o=xml",
+  false
+)
